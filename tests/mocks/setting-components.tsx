e6423862a0cf72d4bc75288b31ff7cdfa868cccc
Jonathan Houck <jhouck@Jonathans-Mac-Studio.local>
2026-01-15 13:53:32 -0800

    fix: Resolve lint errors in test mocks

--- tests/mocks/setting-components.tsx
+++ tests/mocks/setting-components.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 export const SettingSection = (props: any) => <div title={props.title}>{props.children}</div>
 export const SettingRow = (props: any) => <div><label>{props.label}</label>{props.children}</div>
